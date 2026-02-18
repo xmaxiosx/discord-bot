b95ec0f563ec3bb1b7081c12e5685e42ae614354
kevin <aboue.kevin100@outlooK.fr>
2026-02-18 11:47:15 +0100

    Initial clean commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`Connecté en tant que ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.reply('Pong !');
+    }
+});
+
+client.login(process.env.TOKEN);
